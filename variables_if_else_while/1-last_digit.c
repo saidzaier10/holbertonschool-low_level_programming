9d1cc9dd64c4e8f3afc2ef54e5f6f4e3c82b84f8
SaidZaier <8770@holbertonstudents.com>
2024-02-21 23:51:32 +0100

    modulo + structure

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int (n, lastdigit);
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastdigit = n % 10;
+
+	if ()
+	{	printf();
+	}
+
+	else if()
+	{	printf();
+	}
+
+	else if()
+	{	printf();
+	}
+
+	return (0);
+}
