a9f147d3647b32e53bf068e0dc536e99cca3fcd9
SaidZaier <8770@holbertonstudents.com>
2024-02-21 21:26:47 +0100

    RTFM

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,14 +3,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("%d", n);
 
 	if (n > 0)
 	{
@@ -25,9 +30,5 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+return (0);
 }
