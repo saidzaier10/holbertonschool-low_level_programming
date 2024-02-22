f57778985baf9fa2684d4b0bc78464ab0e729ac6
SaidZaier <8770@holbertonstudents.com>
2024-02-22 22:53:25 +0100

    cp task5 task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Point d'entrée du programme
+*
+* Return: Toujours 0 (Succès)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
