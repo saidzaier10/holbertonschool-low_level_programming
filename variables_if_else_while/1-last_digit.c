539a50d6accc51b5b5c8c5dfd89fc2a0abf54285
SaidZaier <8770@holbertonstudents.com>
2024-02-22 11:40:28 +0100

    delete char a

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,21 +13,21 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
+	lstdigit = n % 10;
 
-	if (lastdigit > 5)
+	if (lstdigit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lstdigit);
 	}
 
-	else if (lastdigit == 0)
+	else if (lstdigit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, lstdigit);
 	}
 
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstdigit);
 	}
 
 	return (0);
