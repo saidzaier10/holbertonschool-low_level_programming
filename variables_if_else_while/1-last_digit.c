56d056800ffe9c6266764aabad58d69f71dd838a
SaidZaier <8770@holbertonstudents.com>
2024-02-22 11:29:41 +0100

    trailing white space

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,17 +16,17 @@ int main(void)
 	lastdigit = n % 10;
 
 	if (lastdigit > 5)
-	{	
+	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 	}
 
 	else if (lastdigit == 0)
-	{	
+	{
 		printf("Last digit of %d is %d ans is 0\n", n, lastdigit);
 	}
 
 	else
-	{	
+	{
 		printf("Last digit of %d is %d less than 6 and not 0\n", n, lastdigit);
 	}
 
