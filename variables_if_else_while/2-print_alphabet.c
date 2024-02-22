74586c750168ac08314668bf7a82dcdd2a79ac70
SaidZaier <8770@holbertonstudents.com>
2024-02-22 19:30:50 +0100

    while

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i = i + 1;
+	}
+	return (0);
+}
