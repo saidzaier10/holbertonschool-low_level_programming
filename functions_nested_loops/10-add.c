8b37f53eb07aa360648edf5d78b896527c87d408
SaidZaier <8770@holbertonstudents.com>
2024-02-25 23:39:50 +0100

    task 9,10,11

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two integers
+ *
+ * @i: integer argument
+ * @j: integer argument
+ * Return: Sum
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_to_98 - print numbers a to 98
+ * @n: int argument
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n > 98)
+		{
+			n--;
+		}
+		else
+		{
+			n++;
+		}
+	}
+	printf("%d", 98);
+	printf("\n");
+}
