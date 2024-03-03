0eb2712383a10607f1dc3b63123b7ce082d67c21
SaidZaier <8770@holbertonstudents.com>
2024-03-03 17:42:06 +0100

    functionn tant printss n elements of an array of integers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_array - Prints n elements of an array of integers.
+* @a: The array to print from.
+* @n: The number of elements to print.
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+			printf(", ");
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
