f9d6638d0005b72b77f9b3260bb493b5441f5bb0
SaidZaier <8770@holbertonstudents.com>
2024-03-03 22:26:46 +0100

    Fizz Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+*
+*/
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
