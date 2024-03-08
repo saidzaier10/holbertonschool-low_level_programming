47cc5d7326d059341629868b6cf969bd3d9f80df
SaidZaier <8770@holbertonstudents.com>
2024-03-08 21:12:25 +0100

    gratage de point

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagsums - function
+*
+* @a : pointer a
+* @size : int
+*
+* Return: /
+*/
+
+void print_diagsums(int *a, int size)
+{
+	*a = size;
+}
