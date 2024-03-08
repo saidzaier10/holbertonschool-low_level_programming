ceb762bbc9d6411df086ff2bac059580f1cfd363
SaidZaier <8770@holbertonstudents.com>
2024-03-08 18:27:21 +0100

    directory

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * La fonction principale du programme.
+ *
+ * @param argc Le nombre d'arguments de la ligne de commande.
+ * @param argv Un tableau de pointeurs vers des chaînes de caractères contenant les arguments de la ligne de commande.
+ *
+ * @return 0 en cas de succès, une valeur non nulle en cas d'erreur.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
