a2b50a373c6247b621414f43b4df0ca20a0ada5f
SaidZaier <8770@holbertonstudents.com>
2024-03-08 19:25:20 +0100

    program that prints its name, followed by a new line

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * La fonction principale du programme.
- *
- * @param argc Le nombre d'arguments de la ligne de commande.
- * @param argv Un tableau de pointeurs vers des chaînes de caractères contenant les arguments de la ligne de commande.
- *
- * @return 0 en cas de succès, une valeur non nulle en cas d'erreur.
- */
+* main - prints its name
+*
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
