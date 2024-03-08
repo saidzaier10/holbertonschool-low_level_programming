a6b6fb9e8589057444f30d2936f767d1c1821663
SaidZaier <8770@holbertonstudents.com>
2024-03-08 19:28:53 +0100

    program that prints its name, followed by a new line

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
-* main - prints its name
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point of the program. Prints all command-line arguments.
+ * @argc: The count of command-line arguments. Includes the name of the program
+ * @argv: An array of character pointers listing all the arguments
+ *        argv[0] is the program name, argv[1] is the first argument, and so on
+ *
+ * This program takes any number of command-line arguments, including the name
+ * of the program itself, and prints each one on a new line. It demonstrates
+ * basic usage of command-line arguments in a C program.
+ *
+ * Return: Always returns 0 to signify successful execution.
+ */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int i;
 
+	for (i = 0; i < argc; i++)
+
+	printf("%s\n", argv[i]);
 	return (0);
 }
