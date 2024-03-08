2b7792317f0c0eb2e01e383a2f13d15d0380c4cc
SaidZaier <8770@holbertonstudents.com>
2024-03-08 21:03:20 +0100

    program that multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,43 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h> /* Required for isdigit() function */
 
 /**
-* main - Entry point, program that adds positive numbers.
-* @argc: Argument counter.
-* @argv: Argument variables.
+* main - Multiplies two command-line argument numbers.
+* @argc: Argument count, including program name.
+* @argv: Argument vector, expects two numbers for multiplication.
 *
-* Description: This program takes command line arguments,
-* all expected to be positive numbers, and adds them together.
-* It returns 0 if all inputs are valid numbers, and returns 1
-* if any of the inputs contain a character that is not a digit.
-* It also prints an error message for invalid inputs.
+* Description: If exactly two arguments are provided (excluding the program
+* name), this program calculates and prints their product. Otherwise, it
+* outputs "Error" and returns 1. The arguments are converted from strings
+* to integers using atoi.
 *
-* Return: 0 if successful, 1 if an error is encountered.
+* Return: 0 on success, 1 if incorrect number of arguments.
 */
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int num1, num2;
 
-	if (argc == 1)
+	if (argc <= 2)
 	{
-		printf("0\n");
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
 
-	for (i = 1; i < argc; i++)
-	{
-		j = 0;
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		sum += atoi(argv[i]);
-	}
-	printf("%d\n", sum);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+
 	return (0);
 }
