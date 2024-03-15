1b43c170d389afc7682f59afd985c5a69ea9de8d
SaidZaier <8770@holbertonstudents.com>
2024-03-15 14:29:32 +0100

    update dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - desc
+ * @name: ff
+ * @age: ff
+ * @owner: fff
+*/
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
