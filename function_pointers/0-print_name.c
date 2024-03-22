9e3c4f666a06f9c81010206ecae279156e247388
SaidZaier <8770@holbertonstudents.com>
2024-03-22 23:25:26 +0100

    fix task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,11 +4,10 @@
 * print_name - function that print a name
 * @name: name to print
 * @f: function to use
-* Return: Nothing
+* return : nothing
 */
-
 void print_name(char *name, void(*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name && f)
 		f(name);
 }
